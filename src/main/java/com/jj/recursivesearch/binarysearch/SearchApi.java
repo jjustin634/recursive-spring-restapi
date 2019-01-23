76fc3a8ad5aabbdd4c1325fbfa9478757153760c
Justin Johnson <jjustin634@gmail.com>
2019-01-23 01:02:07 -0600

    first commit

---
+++ src/main/java/com/jj/recursivesearch/binarysearch/SearchApi.java
@@ -0,0 +1,13 @@
+package com.jj.recursivesearch.binarysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApi.class, args);
+    }
+}
+
---
+++ src/test/java/com/jj/recursivesearch/binarysearch/BinarysearchApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jj.recursivesearch.binarysearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BinarysearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
